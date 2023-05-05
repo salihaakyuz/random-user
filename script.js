ad1e3d02cf7baf919b003a04d4ef94c4f5ccd78c
salihaakyuz <200254020@ogr.alanya.edu.tr>
2023-05-05 18:28:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function fetchUser(){
+fetch('https://randomuser.me/api')
+    .then((res)=> res.json())
+    .then((data)=>{
+        displayUser(data.results[0]);
+    });
+}
+function displayUser(user){
+    const userDisplay = document.querySelector('#user');
+    if(user.gender==='female'){
+        document.querySelector('#saliha').style.backgroundColor='steelblue';
+    }else{
+        document.querySelector('#saliha').style.backgroundColor='pink';
+    }
+    userDisplay.innerHTML=
+    `<div class="flex justify-between">
+        <div class="flex">
+          <img
+            class="w-48 h-48 rounded-full mr-8"
+            src=${user?.picture?.large}
+          />
+          <div class="space-y-3">
+            <p class="text-xl">
+              <span class="font-bold">Name: </span>${user.name?.title} ${user.name?.first} ${user.name?.last}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Email: </span>${user.email}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Phone: </span> ${user.phone}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Location: </span>${user.location.city} ${user.location.country}
+            </p>
+            <p class="text-xl"><span class="font-bold">Age: </span>${user.dob.age}</p>
+           </div>
+        </div>
+    </div>`;
+
+}
+document.querySelector('#generate').addEventListener('click', fetchUser);
+fetchUser();
